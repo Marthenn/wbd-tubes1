544d99476266624d399c8ef47cdac186738604bd
Putinabillaa <aidiraputiabil@gmail.com>
2023-09-29 22:08:37 +0700

    chore: add navbar user info

--- src/app/views/templates/navbar_user.php
+++ src/app/views/templates/navbar_user.php
@@ -1,6 +1,14 @@
 <div class="navbar-user">
     <img class="logo" src="<?= BASEURL;?>/img/Logo.svg" alt="logo" >
-    <div class="navbar-titles">
-        <a href="" class="navbar-title">Audio Books</a>
+    <div class="navbar-contents">
+        <div class="titles">
+            <a href="" class="navbar-title">Audio Books</a>
+        </div>
+        <div class="user-info">
+            <a href="" class="user-photo">
+                <img src="<?= BASEURL;?>/img/user-placeholder.svg" class="user-photo-img" alt="User photo"/>
+            </a>
+            <a href="" class="username">Username</a>
+        </div>
     </div>
 </div>
