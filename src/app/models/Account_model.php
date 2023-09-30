1ff6fa538104ce3c13733428aa47b8a6ee6000b0
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-09-30 23:39:20 +0700

    feat (account): made the filter sql query for search bar

--- src/app/models/Account_model.php
+++ src/app/models/Account_model.php
@@ -30,9 +30,16 @@ class Account_model{
         $this->database->execute();
     }
 
-    public function getUserPage($page){
+    public function getUserPage($page, $filter = null){
         $offset = ($page - 1) * 5;
-        $this->database->query("SELECT uid, username, email, joined_date, is_admin FROM account ORDER BY uid ASC LIMIT 5 OFFSET :offset");
+        if (filter == null) {
+            $this->database->query("SELECT uid, username, email, joined_date, is_admin FROM account ORDER BY uid ASC LIMIT 5 OFFSET :offset");
+        }
+        if (filter != null) { // different query to optimize the filter
+            $this->database->query("SELECT uid, username, email, joined_date, is_admin FROM account WHERE username LIKE :filter or email LIKE :filter ORDER BY uid ASC LIMIT 5 OFFSET :offset");
+            $filter = "%".$filter."%";
+            $this->database->bind(":filter", $filter);
+        }
         $this->database->bind(":offset", $offset);
         return $this->database->resultSet();
     }
