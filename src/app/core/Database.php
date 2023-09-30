6ec3671c73c5b36d1aa4a84e240d1e35a37b7416
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-09-30 20:56:05 +0700

    feat (DB): made the DB class with the query execution

--- src/app/core/Database.php
+++ src/app/core/Database.php
@@ -23,8 +23,51 @@ class Database {
         try {
             $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
         } catch(PDOException $e) {
-            $this->error = $e->getMessage();
-            echo $this->error;
+            die($e->getMessage());
         }
+    }
+
+    public function query($query){
+        $this->stmt = $this->dbh->prepare($query);
+    }
+
+    public function bind($param, $value, $type = null){
+        if(is_null($type)){
+            switch(true){
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    public function execute(){
+        $this->stmt->execute();
+    }
+
+    /**
+     * Return all data from the query
+     **/
+    public function resultSet(){
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * Return a single data from the query
+     **/
+    public function single(){
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_ASSOC);
     }
 }
