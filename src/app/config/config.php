83988fb248f1c7d64610d19b5dcab5bf978d5733
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-09-30 19:41:36 +0700

    refactor (config): remove Constants.php and replace it with config.php, also added some DB .env variables to it

---
+++ src/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/tugas-besar-1/src/public'); // path to public folder
+
+// DB
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_USER', getenv('POSTGRES_USER'));
+define('DB_PASS', getenv('POSTGRES_PASSWORD'));
+define('DB_NAME', getenv('POSTGRES_DB'));
--- src/app/core/Constants.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/tugas-besar-1/src/public'); // path to public folder
