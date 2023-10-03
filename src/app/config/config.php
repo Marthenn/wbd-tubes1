8cb3606a77674cbaa5d270b15b1ad1ef76ac0ecc
hanifmz07 <13521157@std.stei.itb.ac.id>
2023-10-03 16:25:26 +0700

    fix: fix env file path

--- src/app/config/config.php
+++ src/app/config/config.php
@@ -1,11 +1,11 @@
 <?php
-$env = parse_ini_file('../../.env');
+$env = parse_ini_file('../.env');
 foreach ($env as $key => $value) {
     putenv("$key=$value");
 }
 
-// define('BASEURL', getenv('BASE_URL')); // path to public folder
-define('BASEURL', 'http://localhost/tugas-besar-1/src/public');
+define('BASEURL', getenv('BASE_URL')); // path to public folder
+// define('BASEURL', 'http://localhost/tugas-besar-1/src/public');
 
 // DB
 define('DB_HOST', getenv('DB_HOST'));
