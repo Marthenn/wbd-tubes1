b92f3ae69118943be5915e1dceb3cc15a92cbfee
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-10-04 03:28:48 +0700

    feat (Book): add the functionality to edit book

--- src/app/models/Book_model.php
+++ src/app/models/Book_model.php
@@ -88,4 +88,71 @@ class Book_model {
         $this->db->bind('offset', $offset);
         return $this->db->resultSet();
     }
+
+    public function getBookPageAdmin($page, $filter = null){
+        // TODO: @Putinabillaa implement mirip atas tapi untuk admin (filter kategori masih belum kebayang soalnya)
+    }
+
+    public function editBook($data) {
+        /**
+         * $data will be key-value pair with keys:
+         * 0. bid (book id)
+         * 1. Title
+         * 2. Author
+         * 3. Rating
+         * 4. Category (singular)
+         * 5. Description
+         * 6. Cover Image Directory (nullable)
+         * 7. Audio Directory (nullable)
+         *
+         * If cover image and audio directory is null, then it will not be updated
+         * If cover image directory value is delete_please, then it will be deleted
+         */
+
+        // finding the aid of the author
+        $this->db->query("SELECT aid FROM author WHERE name = :author");
+        $this->db->bind('author', $data['author']);
+        $aid = $this->db->single()->aid;
+
+        // finding the cid of the category
+        $this->db->query("SELECT cid FROM category WHERE name = :category");
+        $this->db->bind('category', $data['category']);
+        $cid = $this->db->single()->cid;
+
+        // updating the book
+        $query = "UPDATE book SET title = :title, aid = :aid, rating = :rating, category_id = :cid, description = :description";
+        if ($data['cover_image_directory'] != null){
+            if ($data['cover_image_directory'] == 'delete_please'){
+                $query = $query . ", cover_image_directory = NULL";
+            } else {
+                $query = $query . ", cover_image_directory = :cover_image_directory";
+            }
+        }
+        if ($data['audio_directory'] != null){
+            $query = $query . ", audio_directory = :audio_directory";
+        }
+        $query = $query . " WHERE bid = :bid";
+
+        $this->db->query($query);
+        $this->db->bind('title', $data['title']);
+        $this->db->bind('aid', $aid);
+        $this->db->bind('rating', $data['rating']);
+        $this->db->bind('cid', $cid);
+        $this->db->bind('description', $data['description']);
+        if ($data['cover_image_directory'] != null){
+            if ($data['cover_image_directory'] != 'delete_please'){
+                $this->db->bind('cover_image_directory', $data['cover_image_directory']);
+            }
+        }
+        if ($data['audio_directory'] != null){
+            $this->db->bind('audio_directory', $data['audio_directory']);
+        }
+        $this->db->bind('bid', $data['bid']);
+
+        $this->db->execute();
+    }
+
+    public function deleteBook($bid) {
+        // TODO: @HanifMZ implement delete book
+    }
 }
