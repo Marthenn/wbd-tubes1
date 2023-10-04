46424dd7479b69e6da8d8abb484dd2787937c3d3
Putinabillaa <aidiraputiabil@gmail.com>
2023-10-04 18:00:45 +0700

    chore: add filter to admin_list/audiobooks, minor html change sign_up

--- src/app/views/sign_up/index.php
+++ src/app/views/sign_up/index.php
@@ -6,19 +6,19 @@
             <h1>Sign up to WEBWBD</h1>
             <form action="" method="post">
                 <div class="input-field">
-                    <label for="email">Email</label>
+                    <label for="email">Email*</label>
                     <input type="email" name="email" id="email" pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" autocomplete="email" required>
                 </div>
                 <div class="input-field">
-                    <label for="username">Username</label>
+                    <label for="username">Username*</label>
                     <input type="text" name="username" id="username" autocomplete="username" required>
                 </div>
                 <div class="input-field">
-                    <label for="password">Password</label>
+                    <label for="password">Password*</label>
                     <input type="password" name="password" id="password" autocomplete="off" minlength="8" required>
                 </div>
                 <div class="input-field">
-                    <label for="confirm-password">Confirm Password</label>
+                    <label for="confirm-password">Confirm Password*</label>
                     <input type="password" name="confirm-password" id="confirm-password" autocomplete="off" minlength="8" required>
                 </div>
                 <div class="submit-button">
