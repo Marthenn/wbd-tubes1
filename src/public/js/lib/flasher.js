55d3f517993fe8b51ed5bb8f1807835a95cc1702
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-10-07 14:47:44 +0700

    feat (flash): fix the typo of left_button in the third param and add the functionality also attached to the parameter

--- src/public/js/lib/flasher.js
+++ src/public/js/lib/flasher.js
@@ -1,4 +1,4 @@
-const make_flash = (message, type = 'info', left_button = null, left_button = null) => {
+const make_flash = (message, type = 'info', right_button = {text : 'OK'}, left_button = null) => {
     const flash = document.createElement('div');
     flash.classList.add('flash-message');
     flash.classList.add('flash');
@@ -18,13 +18,21 @@ const make_flash = (message, type = 'info', left_button = null, left_button = nu
 
     const right_button_element = document.createElement('button');
     right_button_element.classList.add('flash-message-right-button');
-    right_button_element.innerHTML = right_button;
+    right_button_element.innerHTML = right_button.text;
 
     if (left_button){
         const left_button_element = document.createElement('button');
         left_button_element.classList.add('flash-message-left-button');
-        left_button_element.innerHTML = left_button;
+        left_button_element.innerHTML = left_button.text;
         flash_buttons.appendChild(left_button_element);
+
+        if (!left_button.functionality){ // make so the left button will close the flash
+            left_button_element.addEventListener('click', () => {
+                flash.remove();
+            }
+        )} else {
+            left_button_element.addEventListener('click', left_button.functionality);
+        }
     }
     flash_buttons.appendChild(right_button_element);
 
@@ -32,6 +40,14 @@ const make_flash = (message, type = 'info', left_button = null, left_button = nu
     flash_container.appendChild(flash_buttons);
 
     flash.appendChild(flash_container);
+
+    if (!right_button.functionality){ // make so the right button will close the flash
+        right_button_element.addEventListener('click', () => {
+            flash.remove();
+        })
+    } else {
+        right_button_element.addEventListener('click', right_button.functionality);
+    }
 
     return flash;
 }
